de81bf8afa1c8f6fdb23cb526479a5e7cf876de8
Vaibhav Dubay <sirvaibhavdubay@gmail.com>
2024-03-31 15:01:00 +0530

    chore: removed unused variables

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,29 +1,16 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  Post,
-  Res,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiTags } from '@nestjs/swagger';
 import { LoginDto, LoginResponse } from './dtos/login.dto';
 import { User } from '@sc-decorators/user';
 import { AuthService } from './auth.service';
-import { Response } from 'express';
-import { ConfigService } from '@nestjs/config';
 import { Role } from '@sc-enums/role';
 import { Auth } from '@sc-decorators/auth';
 
 @Controller('auth')
 @ApiTags('Auth')
 export class AuthController {
-  constructor(
-    private authService: AuthService,
-    private configService: ConfigService,
-  ) {}
+  constructor(private authService: AuthService) {}
 
   @Post(':role/login')
   @UseGuards(AuthGuard('local'))
@@ -31,13 +18,8 @@ export class AuthController {
     @Param('role') role: Role,
     @Body() _req: LoginDto,
     @User() user: User,
-    @Res({ passthrough: true }) res: Response,
   ): Promise<LoginResponse> {
     const response = this.authService.generateToken(user);
-    res.cookie('authorization', 'Brearer ' + response.accessToken, {
-      httpOnly: true,
-      secure: true,
-    });
     return response;
   }
 
