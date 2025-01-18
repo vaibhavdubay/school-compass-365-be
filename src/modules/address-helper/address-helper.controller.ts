43fe23b9254986a70f5d9d48fdc33a32127c0e84
Vaibhav Dubay <sirvaibhavdubay@gmail.com>
2025-01-18 12:38:29 +0530

    updated access for address helper

--- src/modules/address-helper/address-helper.controller.ts
+++ src/modules/address-helper/address-helper.controller.ts
@@ -2,8 +2,10 @@ import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AddressHelperService } from './address-helper.service';
 import { Address, SearchKey } from './dto/address-helper.dto';
 import { ApiParam, ApiQuery } from '@nestjs/swagger';
+import { Auth } from '@sc-decorators/auth';
 
 @Controller('address-helper')
+@Auth('all')
 export class AddressHelperController {
   constructor(private readonly addressHelperService: AddressHelperService) {}
 
